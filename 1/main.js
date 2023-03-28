618027702629f304e6720a4066417cee13a7c05f
SimoneScordato99 <scordato.simone@yahoo.com>
2023-03-28 10:58:24 +0200

    start

---
+++ 1/main.js
@@ -0,0 +1,9 @@
+
+let num1 = prompt("enter first number")
+let num2 = prompt("enter first number")
+console.log(num1,num2)
+if(num1 > num2){
+    document.writeln("il primo numero è il migliore")
+} else {
+    document.writeln("il secondo numero è il migliore")
+}
